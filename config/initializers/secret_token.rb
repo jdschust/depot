6ae8f2fd738da46ffb36601f3e909f0e8b0d8a23
John S <jdschusta@gmail.com>
2013-05-18 01:25:07 -0400

    Committing all starting source code for the ruby app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '4f542cd194d68f1b796dff93b5e6e272695db4894c900ebf53264acf8bf45f8609d4a87b8f734210e80085817e9ec8a3a0db5a39cbb01169276eb982959f2982'
